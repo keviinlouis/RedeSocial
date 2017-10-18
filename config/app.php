b92928f506e92981ed68247a3bf9a9ca6df0b7d7
Kevin Louis <keviinlouiis@gmail.com>
2017-10-18 13:50:56 -0200

    fix minor configs

--- config/app.php
+++ config/app.php
@@ -178,7 +178,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
